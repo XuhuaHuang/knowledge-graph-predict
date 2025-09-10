afaa8832950109ea6f721de0dd9e4c64981d2975
XuhuaHuang <xuhuahuang0412@gmail.com>
2025-09-10 16:56:19 -0400

    Update tools.py

--- model/tools.py
+++ model/tools.py
@@ -17,61 +17,95 @@ from torch_scatter import scatter_add
 
 np.set_printoptions(precision=4)
 
-def set_gpu(gpus):
-	os.environ["CUDA_DEVICE_ORDER"]    = "PCI_BUS_ID"
-	os.environ["CUDA_VISIBLE_DEVICES"] = gpus
+
+def set_gpu(gpus) -> None:
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpus
+
 
 def get_logger(name, log_dir, config_dir):
-	config_dict = json.load(open( config_dir + 'log_config.json'))
-	config_dict['handlers']['file_handler']['filename'] = log_dir + name.replace(':', '-')
-	logging.config.dictConfig(config_dict)
-	logger = logging.getLogger(name)
+    config_dict = json.load(open(config_dir + "log_config.json"))
+    config_dict["handlers"]["file_handler"]["filename"] = log_dir + name.replace(
+        ":", "-"
+    )
+    logging.config.dictConfig(config_dict)
+    logger = logging.getLogger(name)
 
-	std_out_format = '%(asctime)s - [%(levelname)s] - %(message)s'
-	consoleHandler = logging.StreamHandler(sys.stdout)
-	consoleHandler.setFormatter(logging.Formatter(std_out_format))
-	logger.addHandler(consoleHandler)
+    std_out_format = "%(asctime)s - [%(levelname)s] - %(message)s"
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setFormatter(logging.Formatter(std_out_format))
+    logger.addHandler(consoleHandler)
 
-	return logger
+    return logger
 
-def get_combined_results(left_results, right_results):
-	results = {}
-	count   = float(left_results['count'])
-
-	results['left_mr']	= round(left_results ['mr'] /count, 5)
-	results['left_mrr']	= round(left_results ['mrr']/count, 5)
-	results['right_mr']	= round(right_results['mr'] /count, 5)
-	results['right_mrr']	= round(right_results['mrr']/count, 5)
-	results['mr']		= round((left_results['mr']  + right_results['mr']) /(2*count), 5)
-	results['mrr']		= round((left_results['mrr'] + right_results['mrr'])/(2*count), 5)
-
-	for k in range(10):
-		results['left_hits@{}'.format(k+1)]	= round(left_results ['hits@{}'.format(k+1)]/count, 5)
-		results['right_hits@{}'.format(k+1)]	= round(right_results['hits@{}'.format(k+1)]/count, 5)
-		results['hits@{}'.format(k+1)]		= round((left_results['hits@{}'.format(k+1)] + right_results['hits@{}'.format(k+1)])/(2*count), 5)
-	return results
 
 def get_combined_results(left_results, right_results):
-	results = {}
-	count   = float(left_results['count'])
+    results = {}
+    count = float(left_results["count"])
 
-	results['left_mr']	= round(left_results ['mr'] /count, 5)
-	results['left_mrr']	= round(left_results ['mrr']/count, 5)
-	results['right_mr']	= round(right_results['mr'] /count, 5)
-	results['right_mrr']	= round(right_results['mrr']/count, 5)
-	results['mr']		= round((left_results['mr']  + right_results['mr']) /(2*count), 5)
-	results['mrr']		= round((left_results['mrr'] + right_results['mrr'])/(2*count), 5)
+    results["left_mr"] = round(left_results["mr"] / count, 5)
+    results["left_mrr"] = round(left_results["mrr"] / count, 5)
+    results["right_mr"] = round(right_results["mr"] / count, 5)
+    results["right_mrr"] = round(right_results["mrr"] / count, 5)
+    results["mr"] = round((left_results["mr"] + right_results["mr"]) / (2 * count), 5)
+    results["mrr"] = round(
+        (left_results["mrr"] + right_results["mrr"]) / (2 * count), 5
+    )
 
-	for k in range(10):
-		results['left_hits@{}'.format(k+1)]	= round(left_results ['hits@{}'.format(k+1)]/count, 5)
-		results['right_hits@{}'.format(k+1)]	= round(right_results['hits@{}'.format(k+1)]/count, 5)
-		results['hits@{}'.format(k+1)]		= round((left_results['hits@{}'.format(k+1)] + right_results['hits@{}'.format(k+1)])/(2*count), 5)
-	return results
+    for k in range(10):
+        results["left_hits@{}".format(k + 1)] = round(
+            left_results["hits@{}".format(k + 1)] / count, 5
+        )
+        results["right_hits@{}".format(k + 1)] = round(
+            right_results["hits@{}".format(k + 1)] / count, 5
+        )
+        results["hits@{}".format(k + 1)] = round(
+            (
+                left_results["hits@{}".format(k + 1)]
+                + right_results["hits@{}".format(k + 1)]
+            )
+            / (2 * count),
+            5,
+        )
+    return results
+
+
+def get_combined_results(left_results, right_results):
+    results = {}
+    count = float(left_results["count"])
+
+    results["left_mr"] = round(left_results["mr"] / count, 5)
+    results["left_mrr"] = round(left_results["mrr"] / count, 5)
+    results["right_mr"] = round(right_results["mr"] / count, 5)
+    results["right_mrr"] = round(right_results["mrr"] / count, 5)
+    results["mr"] = round((left_results["mr"] + right_results["mr"]) / (2 * count), 5)
+    results["mrr"] = round(
+        (left_results["mrr"] + right_results["mrr"]) / (2 * count), 5
+    )
+
+    for k in range(10):
+        results["left_hits@{}".format(k + 1)] = round(
+            left_results["hits@{}".format(k + 1)] / count, 5
+        )
+        results["right_hits@{}".format(k + 1)] = round(
+            right_results["hits@{}".format(k + 1)] / count, 5
+        )
+        results["hits@{}".format(k + 1)] = round(
+            (
+                left_results["hits@{}".format(k + 1)]
+                + right_results["hits@{}".format(k + 1)]
+            )
+            / (2 * count),
+            5,
+        )
+    return results
+
 
 def get_param(shape):
-	param = Parameter(torch.Tensor(*shape)); 	
-	xavier_normal_(param.data)
-	return param
+    param = Parameter(torch.Tensor(*shape))
+    xavier_normal_(param.data)
+    return param
+
 
 def com_mult(a, b):
 	r1, i1 = a[..., 0], a[..., 1]
